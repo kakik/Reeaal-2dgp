37b33b729ce25c06499fae80c79d5a583910e958
kakik <39730096+kakik@users.noreply.github.com>
2018-10-04 17:57:50 +0900

    Drill 08 commit_03
    start drill 08_2

---
+++ Drills/Drill 08/Drill 08_2.py
@@ -0,0 +1,32 @@
+from pico2d import *
+import random
+
+
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+
+open_canvas()
+animation_sheet = load_image("animation_sheet.png")
+KPU_GROUND = load_image("KPU_GROUND.png")
+
+
+frame = 0
+
+def draw_character(point_1,point_2,x,y):
+    pass
+
+
+
+def draw_curve_point(points_0,points_1,points_2,points_3):
+    pass
+
+
+n = 2
+size = 4
+
+points = [(random.randint(0, 800), random.randint(0, 600)) for i in range(size)]
+
+while True:
+    pass
+
+
+
