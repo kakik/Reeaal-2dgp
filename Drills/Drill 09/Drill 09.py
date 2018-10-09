533948274c7c5dca3ff2fab09c44e84a96a72940
kakik <39730096+kakik@users.noreply.github.com>
2018-10-09 18:41:53 +0900

    Drill 09 commit 01
    initial setting

---
+++ Drills/Drill 09/Drill 09.py
@@ -0,0 +1,14 @@
+from pico2d import *
+import random
+
+
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+
+open_canvas()
+
+run_animation = load_image("run_animation.png")
+grass = load_image("grass.png")
+ball_1 = load_image("ball41x41.png")
+ball_2 = load_image("ball21x21.png")
+
+
